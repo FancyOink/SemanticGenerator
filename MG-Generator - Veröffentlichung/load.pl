5b1448468262731e8f6f847bb510c15f8a8071e8
Johannes Kuhn <54399034+FancyOink@users.noreply.github.com>
2025-01-21 14:40:03 +0100

    added a load file

---
+++ MG-Generator - Veröffentlichung/load.pl
@@ -0,0 +1,24 @@
+% file: load.pl
+% origin author : J. Kuhn
+% origin date: November 2024
+% purpose: load file for the semantic generator
+
+:- set_prolog_flag(encoding,utf8).
+:- use_module(library(ordsets)).
+:- use_module(library(ugraphs)).
+:- op(500, xfy, ::). % infix predicate for lexical items
+:- op(500, fx, =). % for selection features
+%:- op(500, fx, #). % for category features, if the need arises to redefine the sign of Category features
+
+:- ['select'].
+:- ['fillIn'].
+:- ['spellOut'].
+:- ['lambdaWorkspace'].
+:- ['helpers/painter'].
+:- ['helpers/stuff'].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Used Lexikon for the generator
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+:- ['grammars/German'].
